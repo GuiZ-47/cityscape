ee1a20502bfa9cace910b4c0d50732b2d83d08ea
GuiZ-47 <guillaume.engel@hotmail.fr>
2024-03-01 09:17:39 +0100

    01.03.24

---
+++ src/DataFixtures/CategoryFixture.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {   
+        // WIP pour Fixture
+        // $category =[
+        //     Pages =>['Property', 'Property Sidebar', 'Property Details','Add new Listing','About Us','FAQ', 'Checkout','Cart','Rent'],
+        //     Project =>['Project Side','Project Details'],
+        //     Blog =>['Blog Classic','Blog Details'],
+        //     Contact,
+        // ];
+        
+        // $c=[
+        //     1=>[
+        //         'title'=>'',
+        //         'slug'=> '',
+        //         'description'=> '',
+        //         'parent'=> [
+        //             'fff'=>'',
+        //         ],
+        //         ],
+        // ];
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
+
+$category =[
+    [Home =>],
+    [Vente =>],
+];
+    
